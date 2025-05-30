1f6db71ffd851f869dd8dfee6b7885d384a5b478
Nguyen Truong Giang <giangliken@gmail.com>
2025-05-30 23:03:45 +0700

    Khoi tao du an web dau tien

---
+++ SPACEGO E-COMMERCE WEBSITE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SPACEGO_E_COMMERCE_WEBSITE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
