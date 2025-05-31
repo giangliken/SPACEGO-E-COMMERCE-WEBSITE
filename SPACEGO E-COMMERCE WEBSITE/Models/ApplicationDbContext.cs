2998bd6313d8965c1b43428fc13132ad0306ee48
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-01 01:23:03 +0700

    Thêm lớp Customer và thêm lớp DbContext để tương tác với CSDL

---
+++ SPACEGO E-COMMERCE WEBSITE/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
