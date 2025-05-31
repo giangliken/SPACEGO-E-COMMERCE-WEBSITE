35dab0a785c2da34268579402ed2f49662cf3cd8
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-01 01:23:51 +0700

    Thêm khởi tạo ban đầu cho program

--- SPACEGO E-COMMERCE WEBSITE/Program.cs
+++ SPACEGO E-COMMERCE WEBSITE/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using SPACEGO_E_COMMERCE_WEBSITE.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
