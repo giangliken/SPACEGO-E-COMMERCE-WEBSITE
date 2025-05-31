1a54c0d630d7b8b2bdef84a57fa2b39391ebe131
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-01 01:55:59 +0700

    Tạo controller login

---
+++ SPACEGO E-COMMERCE WEBSITE/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
