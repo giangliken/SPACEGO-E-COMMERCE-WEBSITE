98bf5c1db414f4e320514f409893262ca9a91615
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-08 22:24:03 +0700

    Thêm lớp ApplicationUserManager  để hiển thị FullName ngoài các phương thức trong lớp UserManager

---
+++ SPACEGO E-COMMERCE WEBSITE/Models/ApplicationUserManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Models
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(
+        IUserStore<ApplicationUser> store,
+        IOptions<IdentityOptions> optionsAccessor,
+        IPasswordHasher<ApplicationUser> passwordHasher,
+        IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+        IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+        ILookupNormalizer keyNormalizer,
+        IdentityErrorDescriber errors,
+        IServiceProvider services,
+        ILogger<UserManager<ApplicationUser>> logger)
+        : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+        }
+        public async Task<string?> GetFullnameAsync(ClaimsPrincipal principal)
+        {
+            var user = await GetUserAsync(principal);
+            return user?.FullName;
+        }
+    }
+}
