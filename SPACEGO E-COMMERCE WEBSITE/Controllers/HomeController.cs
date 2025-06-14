9bd6040d91a5d2f0ec0764680391ddc72276d3ad
tai vo <votai2804@gmail.com>
2025-06-14 23:47:43 +0700

    thu nghiem voi homeindexviewmodel chua xac thuc do ko ket noi dc database

--- SPACEGO E-COMMERCE WEBSITE/Controllers/HomeController.cs
+++ SPACEGO E-COMMERCE WEBSITE/Controllers/HomeController.cs
@@ -2,29 +2,52 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SPACEGO_E_COMMERCE_WEBSITE.Models;
+using SPACEGO_E_COMMERCE_WEBSITE.Repository;
 
 namespace SPACEGO_E_COMMERCE_WEBSITE.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationUserManager _userManager;
+        //private readonly ILogger<HomeController> _logger;
+        //private readonly ApplicationUserManager _userManager;
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IBrandRepository _brandRepository;
+        private readonly IProductImageRepository _productImageRepositoryproductImage;
+        private readonly IReviewRepository _reviewRepositoryreview;
+        private readonly ICartItemRepository _cartItemRepositorycartItem;
+        private readonly IDetailCartItemRepository _detailCartItemRepositorydetailCartItem;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationUserManager userManager)
+        public HomeController(IProductRepository productRepository, ICategoryRepository categoryRepository,
+                              IBrandRepository brandRepository, IProductImageRepository productImageRepositoryproductImage,
+                              IReviewRepository reviewRepositoryreview, ICartItemRepository cartItemRepositorycartItem,
+                              IDetailCartItemRepository detailCartItemRepositorydetailCartItem)
         {
-            _logger = logger;
-            _userManager = userManager;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _brandRepository = brandRepository;
+            _productImageRepositoryproductImage = productImageRepositoryproductImage;
+            _reviewRepositoryreview = reviewRepositoryreview;
+            _cartItemRepositorycartItem = cartItemRepositorycartItem;
+            _detailCartItemRepositorydetailCartItem = detailCartItemRepositorydetailCartItem;
+
+
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var user = _userManager.GetUserAsync(User).Result;
-            if (user != null)
+            var products = await _productRepository.GetAllAsync();
+            var brands = await _brandRepository.GetAllAsync();
+            var categories = await _categoryRepository.GetAllAsync();
+
+            var model = new HomeIndexViewModel
             {
-                var fullname = user.FullName;
-                // code khác
-            }
-            return View();
+                Products = products,
+                Categories = categories,
+                Brands = brands
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ SPACEGO E-COMMERCE WEBSITE/Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SPACEGO_E_COMMERCE_WEBSITE.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Brand> Brands { get; set; }
+    }
+}
