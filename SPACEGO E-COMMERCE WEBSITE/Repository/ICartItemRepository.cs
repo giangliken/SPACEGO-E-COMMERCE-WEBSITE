1193b7bad82252c2db2a3bb13afe3662e3b8015b
tai vo <votai2804@gmail.com>
2025-06-17 22:05:12 +0700

    chinh lai cac lenh tuong tac len database

--- SPACEGO E-COMMERCE WEBSITE/Repository/ICartItemRepository.cs
+++ SPACEGO E-COMMERCE WEBSITE/Repository/ICartItemRepository.cs
@@ -6,9 +6,10 @@ namespace SPACEGO_E_COMMERCE_WEBSITE.Repository
     {
         Task<IEnumerable<CartItem>> GetAllAsync();
         Task<CartItem> GetByIdAsync(int id);
-        Task<IEnumerable<CartItem>> GetByUserIdAsync(string userId);
+        Task<CartItem> GetActiveCartByUserIdAsync(string userId);
         Task AddAsync(CartItem cartItem);
         Task UpdateAsync(CartItem cartItem);
         Task DeleteAsync(int id);
+        Task AddOrUpdateCartItemAsync(string userId, int productId, int quantity);
     }
 }
