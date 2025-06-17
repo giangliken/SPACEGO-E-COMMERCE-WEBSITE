674cd5a905b6c26f4e76ebdc2a8b53c2e13b8182
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-17 13:03:44 +0700

    Eidt Model Product

--- SPACEGO E-COMMERCE WEBSITE/Models/Product.cs
+++ SPACEGO E-COMMERCE WEBSITE/Models/Product.cs
@@ -11,16 +11,26 @@ namespace SPACEGO_E_COMMERCE_WEBSITE.Models
         [StringLength(100)]
         [DisplayName("Tên Sản Phẩm")]
         public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
+        public string? ProductDescription { get; set; }
+        [Required(ErrorMessage ="Giá sản phẩm là bắt buộc")]
         [Range(0, 1000000000)]
-        public decimal? ProductPrice { get; set; }
+        [DisplayName("Giá Sản Phẩm")]
+        public decimal ProductPrice { get; set; }
+        [Required(ErrorMessage ="Số lượng sản phẩm là bắt buộc")]
+        [Range(1, int.MaxValue, ErrorMessage = "...")]
+        [DisplayName("Số Lượng Sản Phẩm")]
         public int ProductQuantity { get; set; }
+        [Required(ErrorMessage ="Mã danh mục là bắt buộc")]
+        [DisplayName("Mã Danh Mục")]
         public int CategoryId { get; set; }
-        public Category? Category { get; set; }
+        public Category Category { get; set; }
+
+        [Required(ErrorMessage ="Mã thương hiệu là bắt buộc")]
+        [DisplayName("Mã Thương Hiệu")]
         public int BrandId { get; set; }
-        public Brand? Brand { get; set; }
+        public Brand Brand { get; set; }
         public string? ImageUrl {  get; set; }
-        public List<ProductImage> ImageUrls { get; set; }
+        public List<ProductImage>? ImageUrls { get; set; }
         public bool isAvailable  { get; set; }
     }
 }
