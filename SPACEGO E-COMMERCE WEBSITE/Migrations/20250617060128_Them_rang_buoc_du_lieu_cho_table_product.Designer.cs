8eeadefd8715ac6c3a19b0e9c7f7ed24bf179250
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-17 13:03:28 +0700

    Thêm ràng buộc dữ liệu cho table Product

--- SPACEGO E-COMMERCE WEBSITE/Migrations/20250617060128_Them_rang_buoc_du_lieu_cho_table_product.Designer.cs
+++ SPACEGO E-COMMERCE WEBSITE/Migrations/20250617060128_Them_rang_buoc_du_lieu_cho_table_product.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SPACEGO_E_COMMERCE_WEBSITE.Models;
 
@@ -11,9 +12,11 @@ using SPACEGO_E_COMMERCE_WEBSITE.Models;
 namespace SPACEGO_E_COMMERCE_WEBSITE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250617060128_Them_rang_buoc_du_lieu_cho_table_product")]
+    partial class Them_rang_buoc_du_lieu_cho_table_product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SPACEGO E-COMMERCE WEBSITE/Migrations/20250617060128_Them_rang_buoc_du_lieu_cho_table_product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Them_rang_buoc_du_lieu_cho_table_product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
