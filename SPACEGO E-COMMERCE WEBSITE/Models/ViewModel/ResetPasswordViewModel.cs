84c8b6300a063bbb3ab92f7b831b64d4da6a2945
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-27 00:31:15 +0700

    Thêm Model Reset mật khẩu

---
+++ SPACEGO E-COMMERCE WEBSITE/Models/ViewModel/ResetPasswordViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SPACEGO_E_COMMERCE_WEBSITE.Models.ViewModel
+{
+    public class ResetPasswordViewModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+    }
+}
