9e634a10e687738f8fe2015cf650482abf790ddc
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-27 00:31:35 +0700

    Email Sender

---
+++ SPACEGO E-COMMERCE WEBSITE/Repository/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Repository
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(
+                    _config["Email:Username"],
+                    _config["Email:Password"]
+                ),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_config["Email:Username"], "SPACEGO Admin"),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
---
+++ SPACEGO E-COMMERCE WEBSITE/Repository/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace SPACEGO_E_COMMERCE_WEBSITE.Repository
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string toEmail, string subject, string htmlMessage);
+    }
+}
