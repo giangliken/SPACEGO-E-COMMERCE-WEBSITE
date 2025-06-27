a5a207ba19b12c1a38675a2ff893df32bdc396a5
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-27 15:29:44 +0700

    View Model nhập kho

---
+++ SPACEGO E-COMMERCE WEBSITE/Models/ViewModel/WarehouseInputViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Models.ViewModel
+{
+    public class WarehouseInputViewModel
+    {
+        public int? VariantId { get; set; }
+        public int ProductId { get; set; }
+
+        [BindNever]
+        public string? ProductName { get; set; }
+
+        [BindNever]
+        public string VariantDisplay { get; set; } = "-";
+
+        [BindNever]
+        public int CurrentQuantity { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Số lượng nhập phải lớn hơn 0")]
+
+        public int InputQuantity { get; set; }
+
+        public bool IsVariant { get; set; }
+    }
+}
---
+++ SPACEGO E-COMMERCE WEBSITE/Models/ViewModel/WarehouseItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SPACEGO_E_COMMERCE_WEBSITE.Models.ViewModel
+{
+    public class WarehouseItemViewModel
+    {
+        public int? VariantId { get; set; } // null nếu là sản phẩm không biến thể
+        public string ProductName { get; set; }
+
+        public string ProductId { get; set; }
+        public string VariantDisplay { get; set; } // "Màu - Dung lượng" hoặc "-"
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; } // Còn hàng, Sắp hết, Hết hàng
+
+    }
+}
