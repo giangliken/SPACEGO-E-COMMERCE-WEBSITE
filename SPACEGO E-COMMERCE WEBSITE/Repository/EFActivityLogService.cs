0242d78431a3e63d307ff1113ed75b2c72675d94
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-27 16:10:08 +0700

    interface activityLogs

---
+++ SPACEGO E-COMMERCE WEBSITE/Repository/EFActivityLogService.cs
@@ -0,0 +1,31 @@
+﻿
+using SPACEGO_E_COMMERCE_WEBSITE.Models;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Repository
+{
+    public class EFActivityLogService : IActivityLogService
+    {
+        private readonly ApplicationDbContext _context;
+        public EFActivityLogService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task LogAsync(string userId, string userName, string actionType, string tableName, string objectId, string description)
+        {
+            var log = new ActivityLog
+            {
+                UserId = userId,
+                UserName = userName,
+                ActionType = actionType,
+                TableName = tableName,
+                ObjectId = objectId,
+                Description = description,
+                Timestamp = DateTime.Now
+            };
+
+            _context.ActivityLogs.Add(log);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SPACEGO E-COMMERCE WEBSITE/Repository/IActivityLogService.cs
@@ -0,0 +1,8 @@
+﻿namespace SPACEGO_E_COMMERCE_WEBSITE.Repository
+{
+    public interface IActivityLogService
+    {
+         Task LogAsync(string userId, string userName, string actionType, string tableName, string objectId, string description);
+
+    }
+}
