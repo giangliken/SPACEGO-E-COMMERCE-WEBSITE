ad3352083cf274fcbf71c8a4e7729d0695885426
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-28 18:12:04 +0700

    Thêm Logic cho OrderController

---
+++ SPACEGO E-COMMERCE WEBSITE/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPACEGO_E_COMMERCE_WEBSITE.Repository;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _orderRepository.GetAllAsync();
+            return View(orders);
+        }
+
+    }
+}
