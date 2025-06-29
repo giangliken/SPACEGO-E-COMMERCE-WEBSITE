b0e1f1416ee98cff82767ff8b521a36aa21749ef
tai vo <votai2804@gmail.com>
2025-06-29 09:52:46 +0700

    push lai phan comment

--- SPACEGO E-COMMERCE WEBSITE/Controllers/CommentController.cs
+++ SPACEGO E-COMMERCE WEBSITE/Controllers/CommentController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SPACEGO_E_COMMERCE_WEBSITE.Models;
+using SPACEGO_E_COMMERCE_WEBSITE.Repository;
 
 namespace SPACEGO_E_COMMERCE_WEBSITE.Controllers
 {
+    [Authorize(Roles = SD.Role_Admin)]
     public class CommentController : Controller
     {
-        public IActionResult Index()
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController(ICommentRepository commentRepository)
         {
-            return View();
+            _commentRepository = commentRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var comments = await _commentRepository.GetAllAsync();
+            return View(comments);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var comment = await _commentRepository.GetByIdAsync(id);
+            if (comment == null) return NotFound();
+            return View(comment);
+        }
+
+        public async Task<IActionResult> Toggle(int id)
+        {
+            await _commentRepository.ToggleStatusAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _commentRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Pending()
+        {
+            var pendingComments = await _commentRepository.GetPendingCommentsAsync();
+            return View("Index", pendingComments); // dùng lại view Index
         }
     }
 }
