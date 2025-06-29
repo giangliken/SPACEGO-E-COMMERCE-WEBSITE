7619479243532098dcf807dd002a9e9000e89098
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-29 15:17:37 +0700

    Bổ sung thuộc thuộc tính Cho OrderProduct

--- SPACEGO E-COMMERCE WEBSITE/Models/OrderProduct.cs
+++ SPACEGO E-COMMERCE WEBSITE/Models/OrderProduct.cs
@@ -8,6 +8,8 @@
         public int ProductId { get; set; }
         public Product? Product { get; set; }
 
+        public int? ProductVariantId { get; set; }
+        public ProductVariant? ProductVariant { get; set; }
         public int Quantity { get; set; }
 
     }
