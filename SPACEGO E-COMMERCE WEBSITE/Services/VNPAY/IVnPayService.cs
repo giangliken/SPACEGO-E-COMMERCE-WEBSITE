60eadd969e8afa118abffe2efc15916284b0cd3b
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-30 21:39:55 +0700

    Interface VNPAY và EF VNPAY

---
+++ SPACEGO E-COMMERCE WEBSITE/Services/VNPAY/IVnPayService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using SPACEGO_E_COMMERCE_WEBSITE.Libraries;
+using SPACEGO_E_COMMERCE_WEBSITE.Models;
+
+namespace SPACEGO_E_COMMERCE_WEBSITE.Services.VNPAY
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collection);
+    }
+}
