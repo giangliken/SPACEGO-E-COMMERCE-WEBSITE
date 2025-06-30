ad94b056558db378c61c9e7368f93679bff512c1
Nguyen Truong Giang <giangliken@gmail.com>
2025-06-30 21:39:38 +0700

    Thêm Model VNPAY

---
+++ SPACEGO E-COMMERCE WEBSITE/Models/PaymentInformationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SPACEGO_E_COMMERCE_WEBSITE.Models
+{
+    public class PaymentInformationModel
+    {
+        public string Name { get; set; }
+        public string OrderType { get; set; }
+        public double Amount { get; set; }
+        public string OrderDescription { get; set; }
+        public int OrderId { get; set; }
+    }
+}
